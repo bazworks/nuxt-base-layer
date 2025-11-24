dceba13ca1e33e8333d878227fc0ed368009b440
Parag Sathaye <parag@enine.dev>
2025-11-24 12:49:03 +0530

    Updated main.css path correctly

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
+import { fileURLToPath } from 'node:url'
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@norbiros/nuxt-auto-form'],
-  css: ['~/assets/css/main.css'],
+  css: [fileURLToPath(new URL('./app/assets/css/main.css', import.meta.url))],
   ui: {
     theme: {
       colors: ['primary', 'secondary', 'neutral', 'info', 'success', 'warning', 'error'],
